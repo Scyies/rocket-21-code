d693b22e5dbda13c400b19e3140b2286f16ea5cf
Scyies <scyies3@gmail.com>
2022-08-20 13:29:07 -0300

    edges refinados

--- src/components/NotFoundPage.tsx
+++ src/components/NotFoundPage.tsx
@@ -1,10 +1,10 @@
-import NotFound from '../assets/404Page.png';
+import NotFound from '../assets/notfoundpage.png';
 
 export function NotFoundPage() {
   return (
     <div className='flex flex-col place-items-center justify-center p-5'>
       <p className='text-2xl text-white p-5 font-bold'>Página de erro 404</p>
-      <img src={NotFound} alt="" />
+      <img src={NotFound} alt="" className='aspect-square max-h-[20rem] max-w-[20rem] object-cover place-items-center' />
     </div>
   )
 }
