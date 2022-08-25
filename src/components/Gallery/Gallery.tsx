c5a195639a4831fee5f0c2e38545aede3c47caa7
Scyies <scyies3@gmail.com>
2022-08-25 20:16:57 -0300

    alteração das imagens

--- src/components/Gallery/Gallery.tsx
+++ src/components/Gallery/Gallery.tsx
@@ -3,24 +3,24 @@ import { ImageSlider } from "./ImageSlider";
 export function Gallery() {
   const slides = [
     {
-      url: "https://i.pinimg.com/736x/9a/f5/c1/9af5c1d7bc5941704e9a68907caca4a2.jpg",
-      title: "viado indiano",
+      url: "https://images3.alphacoders.com/641/thumb-1920-641193.jpg",
+      title: "Bloodborne",
     },
     {
       url: "https://images6.alphacoders.com/785/785425.png",
       title: "rin e emyia",
     },
     {
-      url: "https://image.winudf.com/v2/image1/Y29tLmFzdW5hLmFwcC53YWxscGFwZXIuZmF0ZV9zY3JlZW5fMF8xNjM3MjM3MDE2XzAzMA/screen-0.jpg?fakeurl=1&type=.jpg",
-      title: "seiba e kiritsugo",
+      url: "https://i.pinimg.com/originals/40/e9/da/40e9daa6982435261c840673b008b5dd.jpg",
+      title: "aniki",
     },
     {
       url: "https://images6.alphacoders.com/657/657194.jpg",
       title: "seiba",
     },
     {
-      url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSx-SGhWrdnfrIi6d82eavoMadOIPtJLj6_PQ&usqp=CAU",
-      title: "rider",
+      url: "https://wallpaperaccess.com/full/232672.jpg",
+      title: "ds",
     },
   ];
   return (
